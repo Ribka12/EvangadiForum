92177a509131dd195dd97a98848143935ebaa319
eden0330 <edadani1415@gmail.com>
2025-12-29 18:37:21 +0300

    rigister_route

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,3 +1,13 @@
+const express = require("express");
+const router = express.Router();
 
+//user controllers
+const {register,login,checkUser}=require("../controllers/authController");
+
+//register route
+router.post("/register",register) 
+
+
+module.exports = router;
 
 
