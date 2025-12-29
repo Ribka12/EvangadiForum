ddb6ef7870ae3429178f8dfb2d96edb108929849
alem624 <alemtshayatreso@gmail.com>
2025-12-29 19:33:03 +0300

    get answer

--- backend/controllers/answerController.js
+++ backend/controllers/answerController.js
@@ -2,41 +2,25 @@ const answerService = require("../services/answer.service");
 const db = require("../config/db");
 const { StatusCodes } = require("http-status-codes");
 
-const getAnswersByQuestion = async (req, res, next) => {
-  const { question_id } = req.params;
-
-  // Validation
-  if (!question_id || isNaN(question_id)) {
-    return res.status(400).json({
-      success: false,
-      message: "Invalid question_id",
-    });
-  }
-
+// GET ANSWERS FOR QUESTION
+async function getAnswers(req, res) {
   try {
-    const answers = await answerService.getAnswersByQuestionId(question_id);
-
-    if (answers.length === 0) {
-      return res.status(StatusCodes.NOT_FOUND).json({
-        success: false,
-
-        error: "Not Found",
-        message: "The requested question could not be found.",
-      });
-    }
-
-    res.status(200).json({
-      success: true,
-      count: answers.length,
-      data: answers,
-    });
+    const question_id = req.params.question_id;
+     if (!question_id || isNaN(question_id)) {
+       return res
+         .status(StatusCodes.NOT_FOUND)
+         .json({ message: "The requested question could not be found." });
+     }
+    const sql =
+      "SELECT a.*, u.username FROM answerTable a JOIN userTable u ON a.user_id = u.user_id WHERE a.question_id = ? ORDER BY a.created_at ASC";
+    const result = await db.execute(sql, [question_id]);
+    res.status(StatusCodes.OK).json(result[0]);
   } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      error: "Internal Server Error",
-      message: "An unexpected error occurred.",
-    });
-    next(error); // goes to error middleware
+    console.error(error);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: "An unexpected error occurred." });
   }
-};
+}
 
-module.exports = { getAnswersByQuestion };
+module.exports = {getAnswers}
