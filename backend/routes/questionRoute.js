bb1ef369e79930fbfdc46a1707ba9b4639a62daf
Ribka Mengiste <167540456+Ribka12@users.noreply.github.com>
2026-01-04 13:08:24 +0300

    Main updated

--- backend/routes/questionRoute.js
+++ backend/routes/questionRoute.js
@@ -4,9 +4,7 @@ const express = require("express");
 const router = express.Router();
 const questionController = require("../controllers/questionController");
 
-router.use((req, res, next) => {
-  console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
-  next();
+
 
 router.get("/", questionController.getAllQuestions);
 router.get("/:question_id", questionController.getSingleQuestion);
