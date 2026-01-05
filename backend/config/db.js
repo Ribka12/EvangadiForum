88a2efd3574e2ab04cf893f0371030952cdd2618
Ribka Mengiste <167540456+Ribka12@users.noreply.github.com>
2026-01-05 15:19:45 +0300

    Main updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2");
 const dotenv = require("dotenv");
 
-dotenv.config();
+dotenv.config({ path: "../.env" });
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
